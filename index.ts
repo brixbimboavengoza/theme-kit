7d83eaba4a84f4267be21fe6047301959b915b32
Brix Avengoza <brixavengoza@Brixs-MacBook-Pro-2.local>
2024-01-16 12:42:55 +0800

    update main file

---
+++ index.ts
@@ -0,0 +1,2 @@
+export * from '@chakra-ui/react';
+export { default as theme } from './src/themes';
---
+++ src/themes/index.ts
@@ -0,0 +1,7 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const customTheme = extendTheme({
+	//   custom theme
+});
+
+export default customTheme;
